c00c09cc762e9ba7eb5bd0862762e567c6d4ebab
TusharKotian <126941654+TusharKotian@users.noreply.github.com>
2024-07-07 22:40:15 +0530

    Add project files.

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+
+        //CategoryID
+
+        public int Amount {  get; set; }
+        public int Balance { get; set; }
+    }
+}
